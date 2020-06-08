30c8a817f8d93b8ec3d1fc03fcb041aed619d127
l5x <189198+l5x@users.noreply.github.com>
2020-06-08 19:09:10 +0400

    Update husky.config.js

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'ls-lint && terraform validate && terraform fmt && terraform-docs markdown ./ > README.md && git add README.md'
+    'pre-commit': 'ls-lint && terraform validate && terraform fmt && git update-index --again && terraform-docs markdown ./ > README.md && git add README.md'
   }
 }
